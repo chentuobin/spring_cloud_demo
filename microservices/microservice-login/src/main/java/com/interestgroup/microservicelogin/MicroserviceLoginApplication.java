95566b77ef7753c5bcad7deb33e9dc21239c6b27
chentuobin <chentuobin@sina.cn>
2018-12-13 17:15:32 +0800

    added the configuration

--- microservices/microservice-login/src/main/java/com/interestgroup/microservicelogin/MicroserviceLoginApplication.java
+++ microservices/microservice-login/src/main/java/com/interestgroup/microservicelogin/MicroserviceLoginApplication.java
@@ -1,13 +1,23 @@
 package com.interestgroup.microservicelogin;
 
+import com.interestgroup.config.UserSerivceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RibbonClient(name = "MICROSERVICE-USER", configuration = UserSerivceConfiguration.class)
 public class MicroserviceLoginApplication {
-
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceLoginApplication.class, args);
 	}
--- microservices/microservice-login/src/main/java/com/interestgroup/microservicelogin/controller/LoginController.java
+++ microservices/microservice-login/src/main/java/com/interestgroup/microservicelogin/controller/LoginController.java
@@ -1,31 +1,21 @@
 package com.interestgroup.microservicelogin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
-import com.netflix.discovery.EurekaClient;
-
 @RestController
 public class LoginController {
+
 	@Autowired
-	private EurekaClient discoveryClient;
-	
-    @Bean
-	@LoadBalanced
-    RestTemplate restTemplate() {
-       return new RestTemplate();
-   }
-    
+	RestTemplate restTemplate;
+
 	@GetMapping("/{id}")
 	public Object login(@PathVariable Long id) {
-		String url = discoveryClient.getNextServerFromEureka("MICROSERVICE-USER", false).getHomePageUrl();;
-		return restTemplate().getForEntity(url + "/" + id, String.class).getBody();
+		return restTemplate.getForEntity("http://MICROSERVICE-USER/" + id, String.class).getBody();
 	}
 
 }
